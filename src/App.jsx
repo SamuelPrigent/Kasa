7de1bf76e05c116641e6f151a84056cb49decda5
Samuel <samuel.prigent@yahoo.fr>
2023-04-10 19:00:24 +0200

    rename fichiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 // pages
-import HomePage from "./pages/HomePage.jsx";
+import Home from "./pages/Home.jsx";
 import Error from "./pages/Error.jsx"; // page error
 import Logement from "./pages/Logement.jsx"; // page logement
 import About from "./pages/About.jsx"; // page info
@@ -12,7 +12,7 @@ import "./style/reset.css";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<Home />}></Route>
       <Route path="/about" element={<About />}></Route>
       <Route path="/logement" element={<Logement />}></Route>
       <Route path="/logement/:id" element={<Logement />}></Route>
