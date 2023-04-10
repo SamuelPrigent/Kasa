cff6dc1aff119fba3f97a9a5b271cf9acd7e004f
Samuel <samuel.prigent@yahoo.fr>
2023-04-10 17:01:00 +0200

    optimisation du router si la route logement/:id a un id qui n'éxiste pas alors on est redirigé vers la page d'erreur. De plus il n'y a plus de code qui peut être éxécuté si l'on ne trouve pas d'objet avec l'id recherché

--- src/components/Logement.jsx
+++ src/components/Logement.jsx
@@ -11,6 +11,8 @@ import "../style/reset.css";
 import { useParams } from "react-router-dom"; // recup params
 import { useState } from "react";
 import { useEffect } from "react";
+// TEST
+import { useNavigate } from "react-router-dom";
 // component
 import Colapse from "./Colapse.jsx";
 
@@ -39,197 +41,218 @@ function createEquiment(equipements) {
 function Ratings() {
   const { id } = useParams(); // get id
   const kasa = logement.find((element) => element.id === id);
-  const rating = kasa.rating;
-
-  if (rating === "1") {
-    return (
-      <div className="logementRatings">
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-      </div>
-    );
-  }
-  if (rating === "2") {
-    return (
-      <div className="logementRatings">
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-      </div>
-    );
-  }
-  if (rating === "3") {
-    return (
-      <div className="logementRatings">
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-      </div>
-    );
-  }
-  if (rating === "4") {
-    return (
-      <div className="logementRatings">
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingGrey} className="logementRating" alt="grey star" />
-      </div>
-    );
-  }
-  if (rating === "5") {
-    return (
-      <div className="logementRatings">
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-        <img src={RatingRed} className="logementRating" alt="red star" />
-      </div>
-    );
+  if (kasa) {
+    const rating = kasa.rating;
+    if (rating === "1") {
+      return (
+        <div className="logementRatings">
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+        </div>
+      );
+    }
+    if (rating === "2") {
+      return (
+        <div className="logementRatings">
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+        </div>
+      );
+    }
+    if (rating === "3") {
+      return (
+        <div className="logementRatings">
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+        </div>
+      );
+    }
+    if (rating === "4") {
+      return (
+        <div className="logementRatings">
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingGrey} className="logementRating" alt="grey star" />
+        </div>
+      );
+    }
+    if (rating === "5") {
+      return (
+        <div className="logementRatings">
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+          <img src={RatingRed} className="logementRating" alt="red star" />
+        </div>
+      );
+    }
   }
 }
 
 function ImageSlider({ array }) {
-  const [currentImage, setCurrentImage] = useState(array[0]);
-  const [currentNumber, setCurrentNumber] = useState(1);
+  const { id } = useParams(); // get id
+  const kasa = logement.find((element) => element.id === id);
+  if (kasa) {
+    const [currentImage, setCurrentImage] = useState(array[0]);
+    const [currentNumber, setCurrentNumber] = useState(1);
 
-  // visibilité or arrow and text
-  const [arrowVisible, setArrowVisible] = useState(true);
-  useEffect(() => {
-    if (array.length === 1) {
-      setArrowVisible(false);
-    }
-  }, [array]);
-
-  function handleNextImage() {
-    const currentIndex = array.indexOf(currentImage);
-    let nextIndex = currentIndex + 1;
-    if (nextIndex === array.length) {
-      nextIndex = 0;
-    }
-    setCurrentImage(array[nextIndex]);
-    setCurrentNumber(nextIndex + 1);
-  }
-
-  function handlePrevImage() {
-    const currentIndex = array.indexOf(currentImage);
-    let nextIndex = currentIndex - 1;
-    if (nextIndex === -1) {
-      nextIndex = array.length - 1;
-    }
-    setCurrentImage(array[nextIndex]);
-    setCurrentNumber(nextIndex + 1);
-  }
-
-  // Nav via KeyDown
-  useEffect(() => {
-    function handleKeyDown(e) {
-      if (e.keyCode === 37) {
-        handlePrevImage();
+    // visibilité or arrow and text
+    const [arrowVisible, setArrowVisible] = useState(true);
+    useEffect(() => {
+      if (array.length === 1) {
+        setArrowVisible(false);
       }
-      if (e.keyCode === 39) {
-        handleNextImage();
-      }
-    }
-    document.addEventListener("keydown", handleKeyDown);
-    return () => document.removeEventListener("keydown", handleKeyDown);
-  }, [currentImage]);
+    }, [array]);
 
-  return (
-    <div className="headerContainerLogement">
-      <div className="bannerContainerLogement">
-        <img
-          src={currentImage}
-          className="bannerImgLogement"
-          alt="photo logement"
-        />
-        <div onClick={handlePrevImage} className="arrowBannerLeftContainer">
+    function handleNextImage() {
+      const currentIndex = array.indexOf(currentImage);
+      let nextIndex = currentIndex + 1;
+      if (nextIndex === array.length) {
+        nextIndex = 0;
+      }
+      setCurrentImage(array[nextIndex]);
+      setCurrentNumber(nextIndex + 1);
+    }
+
+    function handlePrevImage() {
+      const currentIndex = array.indexOf(currentImage);
+      let nextIndex = currentIndex - 1;
+      if (nextIndex === -1) {
+        nextIndex = array.length - 1;
+      }
+      setCurrentImage(array[nextIndex]);
+      setCurrentNumber(nextIndex + 1);
+    }
+
+    // Nav via KeyDown
+    useEffect(() => {
+      function handleKeyDown(e) {
+        if (e.keyCode === 37) {
+          handlePrevImage();
+        }
+        if (e.keyCode === 39) {
+          handleNextImage();
+        }
+      }
+      document.addEventListener("keydown", handleKeyDown);
+      return () => document.removeEventListener("keydown", handleKeyDown);
+    }, [currentImage]);
+
+    return (
+      <div className="headerContainerLogement">
+        <div className="bannerContainerLogement">
           <img
-            src={Arrow}
-            className={`arrowBannerLeft ${arrowVisible ? "" : "hide"}`}
-            alt="fleche précédent"
+            src={currentImage}
+            className="bannerImgLogement"
+            alt="photo logement"
           />
-        </div>
-        <div onClick={handleNextImage} className="arrowBannerRightContainer">
-          <img
-            src={Arrow}
-            className={`arrowBannerRight ${arrowVisible ? "" : "hide"}`}
-            alt="fleche suivant"
-          />
-        </div>
-        <div className={`numberBanner ${arrowVisible ? "" : "hide"}`}>
-          {currentNumber}/{array.length}
+          <div onClick={handlePrevImage} className="arrowBannerLeftContainer">
+            <img
+              src={Arrow}
+              className={`arrowBannerLeft ${arrowVisible ? "" : "hide"}`}
+              alt="fleche précédent"
+            />
+          </div>
+          <div onClick={handleNextImage} className="arrowBannerRightContainer">
+            <img
+              src={Arrow}
+              className={`arrowBannerRight ${arrowVisible ? "" : "hide"}`}
+              alt="fleche suivant"
+            />
+          </div>
+          <div className={`numberBanner ${arrowVisible ? "" : "hide"}`}>
+            {currentNumber}/{array.length}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 // Info Page
 function Info() {
   const { id } = useParams(); // get id
   const kasa = logement.find((element) => element.id === id);
-  const host = kasa.host.name.split(" ");
-  const tags = kasa.tags;
-  const tagElements = createTags(tags);
-  const equipements = kasa.equipments;
-  const stuffElements = createEquiment(equipements);
-  const picsArray = kasa.pictures;
-  const rating = kasa.rating;
+  if (kasa) {
+    const host = kasa.host.name.split(" ");
+    const tags = kasa.tags;
+    const tagElements = createTags(tags);
+    const equipements = kasa.equipments;
+    const stuffElements = createEquiment(equipements);
+    const picsArray = kasa.pictures;
 
-  return (
-    <div>
-      <ImageSlider array={picsArray} />
-      <div className="logementContainer">
-        <div className="logementInfoHeader">
-          <div className="logementInfoHeaderLeft">
-            <div className="logementTitleLocation">
-              <div className="logementTitle">{kasa.title}</div>
-              <div className="logementLocation">{kasa.location}</div>
-            </div>
-            <div className="logementTags">{tagElements}</div>
-          </div>
-          <div className="logementInfoHeaderRight">
-            <div className="logementId">
-              <div className="logementPrenomNom">
-                <div className="logementPrenom">{host[0]} </div>
-                <div className="logementNom"> {host[1]}</div>
+    return (
+      <div>
+        <ImageSlider array={picsArray} />
+        <div className="logementContainer">
+          <div className="logementInfoHeader">
+            <div className="logementInfoHeaderLeft">
+              <div className="logementTitleLocation">
+                <div className="logementTitle">{kasa.title}</div>
+                <div className="logementLocation">{kasa.location}</div>
               </div>
-              <div>
-                <img
-                  src={kasa.host.picture}
-                  className="userImg"
-                  alt="photo propriétaire"
-                />
-              </div>
+              <div className="logementTags">{tagElements}</div>
             </div>
-            <Ratings />
+            <div className="logementInfoHeaderRight">
+              <div className="logementId">
+                <div className="logementPrenomNom">
+                  <div className="logementPrenom">{host[0]} </div>
+                  <div className="logementNom"> {host[1]}</div>
+                </div>
+                <div>
+                  <img
+                    src={kasa.host.picture}
+                    className="userImg"
+                    alt="photo propriétaire"
+                  />
+                </div>
+              </div>
+              <Ratings />
+            </div>
           </div>
-        </div>
-        <div className="logementBloc">
-          <div className="logementBlocDisplay">
-            <Colapse
-              title={"Description"}
-              desc={kasa.description}
-              state={true}
-            />
-          </div>
-          <div className="logementBlocDisplay">
-            <Colapse title={"Équipements"} desc={stuffElements} state={true} />
+          <div className="logementBloc">
+            <div className="logementBlocDisplay">
+              <Colapse
+                title={"Description"}
+                desc={kasa.description}
+                state={true}
+              />
+            </div>
+            <div className="logementBlocDisplay">
+              <Colapse
+                title={"Équipements"}
+                desc={stuffElements}
+                state={true}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    // si l'id n'est pas reconnu redirigé sur la page d'erreur
+    const navigate = useNavigate();
+    useEffect(() => {
+      const kasa = logement.find((element) => element.id === id);
+      if (!kasa) {
+        navigate("/error");
+      }
+    }, [id, logement, navigate]);
+
+    return <div></div>;
+  }
 }
 
 export default Info;
